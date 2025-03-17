449b0f9f3b7e68ed59a08f79f03b6cce230ffbb9
Saveur Bikorimana <bikorimanaxavier@gmail.com>
2025-03-17 09:33:00 +0200

    added splash screen

---
+++ app/(tabs)/community.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const CommunityScreen = () => {
+    return (
+        <View>
+            <Text>Hello world</Text>
+        </View>
+    );
+}
+ 
+export default CommunityScreen;
