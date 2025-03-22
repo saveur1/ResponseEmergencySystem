ff9c38119e27a6cf8f7e61c3ff66b52f2a4e52d3
queen <nsengaqueen123@gmail.com>
2025-03-22 11:44:05 +0200

    removing the incidentDetails css file as it is not in use

--- src/components/incidentDetails/incidentDetails.jsx
+++ src/components/incidentDetails/incidentDetails.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import "./incidentDetails.css";
+
 
 function IncidentDetails() {
   const { id } = useParams();
